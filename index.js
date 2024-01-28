126a10711b19b97b3007b2cac7d44ea4e4fba4af
Md Ismail <mdismaeelkhan345@gmail.com>
2024-01-28 18:49:57 +0530

    frond And Back Catds Added

--- index.js
+++ index.js
@@ -43,75 +43,102 @@ async function allTypeOfPokemon() {
     const response = await fetch(ele.url);
     const pokemonData = await response.json();
     // console.log(pokemonData);
-
-    // Create and manipulate DOM elements based on 'pokemonData'
-    const pokemonCard_Detail = document.createElement("div");
-    const pokemonFront_Card = document.createElement("div");
-    const pokemonBack_Card = document.createElement("div");
-
-    // Creating element Dynamically for frontCard
-    pokemonFront_Card.innerHTML = `
-              <div class="pokeIdes">
-              <p id="idNumber">#${pokemonData.id}</p>
-              <p id="dColor"></p>
-              </div>
-              <figure class="figure">
-                <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}">
-                <figcaption>${pokemonData.name}</figcaption>
-              </figure>
-              <div class="heightAndWidth">
-              <p>height:- ${pokemonData.height}</p>
-              <p>weight:- ${pokemonData.weight}</p>
-              </div>`;
-
-    const typeData = document.createElement("div");
-
-    pokemonData.types.forEach((e) => {
-      let p = document.createElement("p");
-
-      p.innerText = e.type.name;
-      typeData.appendChild(p);
-    });
-    typeData.classList.add("types");
-    pokemonFront_Card.appendChild(typeData);
-    pokemonFront_Card.classList.add("pokemonFrontCard");
-
-    // Creating element Dynamically for frontCard
-    pokemonBack_Card.innerHTML = `
-          <div class="pokeIdesBackCard">
-            <p id="idNumber">#${pokemonData.id}</p>
-            <p id="dColor"></p>
-          </div>
-          <figure class="figureBackCard">
-            <img src="${pokemonData.sprites.back_default}" alt="${pokemonData.name}">
-            <figcaption>${pokemonData.name}</figcaption>
-          </figure>
-          <p class="abilityHeading">ABILITIES:-</p>`;
-
-    const abilityData = document.createElement("div");
-
-    pokemonData.abilities.forEach((e) => {
-      // console.log(e);
-      let p = document.createElement("p");
-      p.innerText = e.ability.name;
-      abilityData.appendChild(p);
-    });
-    abilityData.classList.add("abilities");
-    pokemonBack_Card.appendChild(abilityData);
-    pokemonBack_Card.classList.add("pokemonBackCard");
-
-    // appending the Card inside the pokemonList
-    pokemonCard_Detail.classList.add("pokemonCardDetail");
-    pokemonCard_Detail.append(pokemonFront_Card, pokemonBack_Card);
-    pokemonCard_Detail.classList.add("pokemonCardDetail");
-    pokemonList.appendChild(pokemonCard_Detail);
+    renderCard(pokemonData);
   });
 }
 
-// console.log(searchFilter);
+/*****************************************************************************************
+@param 
+
+@return 
+*****************************************************************************************/
+function renderCard(pokemonData) {
+  // Create and manipulate DOM elements based on 'pokemonData'
+  const innerDiv = document.createElement("div");
+  const pokemonCard_Detail = document.createElement("div");
+  const pokemonFront_Card = document.createElement("div");
+  const pokemonBack_Card = document.createElement("div");
+
+  // Creating element Dynamically for frontCard
+  pokemonFront_Card.innerHTML = `
+    <div class="pokeIdes">
+    <p id="idNumber">#${pokemonData.id}</p>
+    <p id="dColor"></p>
+    </div>
+    <figure class="figure">
+      <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}">
+      <figcaption>${pokemonData.name}</figcaption>
+    </figure>
+    <div class="heightAndWidth">
+    <p>height:- ${pokemonData.height}</p>
+    <p>weight:- ${pokemonData.weight}</p>
+    </div>`;
+
+  const typeData = document.createElement("div");
+
+  pokemonData.types.forEach((e) => {
+    let p = document.createElement("p");
+
+    p.innerText = e.type.name;
+    typeData.appendChild(p);
+  });
+  typeData.classList.add("types");
+  pokemonFront_Card.appendChild(typeData);
+  pokemonFront_Card.classList.add("pokemonFrontCard");
+
+  // Creating element Dynamically for frontCard
+  pokemonBack_Card.innerHTML = `
+    <div class="pokeIdesBackCard">
+      <p id="idNumber">#${pokemonData.id}</p>
+      <p id="dColor"></p>
+    </div>
+    <figure class="figureBackCard">
+      <img src="${pokemonData.sprites.back_default}" alt="${pokemonData.name}">
+      <figcaption>${pokemonData.name}</figcaption>
+    </figure>
+    <p class="abilityHeading">ABILITIES:-</p>`;
+
+  const abilityData = document.createElement("div");
+
+  pokemonData.abilities.forEach((e) => {
+    // console.log(e);
+    let p = document.createElement("p");
+    p.innerText = `${e.ability.name}`;
+    abilityData.appendChild(p);
+  });
+  abilityData.classList.add("abilities");
+  pokemonBack_Card.appendChild(abilityData);
+  pokemonBack_Card.classList.add("pokemonBackCard");
+
+  // appending the Card inside the pokemonList
+  innerDiv.classList.add("innerDiv");
+  pokemonCard_Detail.classList.add("pokemonCardDetail");
+  pokemonCard_Detail.append(pokemonFront_Card, pokemonBack_Card);
+  pokemonCard_Detail.classList.add("pokemonCardDetail");
+  innerDiv.appendChild(pokemonCard_Detail);
+  pokemonList.appendChild(innerDiv);
+}
+
+/*****************************************************************************************
+@param 
+
+@return 
+*****************************************************************************************/
+async function filterSelectTypes() {
+  pokemonList.innerHTML = "";
+  const response = await fetch(optionType.value);
+  const data = await response.json();
+  data.pokemon.forEach(async (element) => {
+    const dataUrl = element.pokemon.url;
+    const response = await fetch(dataUrl);
+    const data = await response.json();
+    renderCard(data);
+  });
+}
+
 searchFilter.addEventListener("click", (event) => {
   event.preventDefault();
-  console.log(optionType.value);
+  filterSelectTypes();
 });
 
 /***********************************
