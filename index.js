deaf13162900ba3faa9b6e7eae2ba9dd434a99e2
Md Ismail <mdismaeelkhan345@gmail.com>
2024-01-30 14:09:27 +0530

    updation

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const optionType = document.querySelector(".optionType");
 const pokemonList = document.querySelector(".pokemonList");
 const searchFilter = document.querySelector("#searchFilter");
-// const input = document.querySelector("#input");
+const searchPokemonInput = document.querySelector("#searchPokemonInput");
+const searchBtn = document.querySelector("#searchBtn");
 
 // optionsType Url:-
 const typeOfUrl = "https://pokeapi.co/api/v2/type/";
@@ -30,6 +31,7 @@ async function createOptions() {
 const apiOfAllPokemon = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=50";
 
 /*****************************************************************************************
+from this function fetching all pokemon 
 @param 
 
 @return 
@@ -48,8 +50,8 @@ async function allTypeOfPokemon() {
 }
 
 /*****************************************************************************************
-@param 
-
+@param pokemonData
+  updating Dom dynamically when the function will call
 @return 
 *****************************************************************************************/
 function renderCard(pokemonData) {
@@ -59,11 +61,14 @@ function renderCard(pokemonData) {
   const pokemonFront_Card = document.createElement("div");
   const pokemonBack_Card = document.createElement("div");
 
-  // Creating element Dynamically for frontCard
+  // Creating element Dynamically for frontCard  base_stat
   pokemonFront_Card.innerHTML = `
     <div class="pokeIdes">
     <p id="idNumber">#${pokemonData.id}</p>
-    <p id="dColor"></p>
+    <div id="fColor">
+      <p>${pokemonData.stats[0].stat.name}</p>
+      <p>${pokemonData.stats[0].base_stat}<p></p>
+      </div>
     </div>
     <figure class="figure">
       <img src="${pokemonData.sprites.front_default}" alt="${pokemonData.name}">
@@ -72,6 +77,20 @@ function renderCard(pokemonData) {
     <div class="heightAndWidth">
     <p>height:- ${pokemonData.height}</p>
     <p>weight:- ${pokemonData.weight}</p>
+    </div>
+    <div class="stats">
+    <div class="state-1">
+      <p>${pokemonData.stats[1].base_stat}<p></p>
+      <p>${pokemonData.stats[1].stat.name}</p>
+    </div>
+    <div class="state-1">
+      <p>${pokemonData.stats[2].base_stat}</p>
+      <p>${pokemonData.stats[2].stat.name}</p>
+    </div>
+    <div class="state-1">
+      <p>${pokemonData.stats[3].base_stat}<p></p>
+      <p>${pokemonData.stats[3].stat.name}</p>
+    </div>
     </div>`;
 
   const typeData = document.createElement("div");
@@ -81,16 +100,19 @@ function renderCard(pokemonData) {
 
     p.innerText = e.type.name;
     typeData.appendChild(p);
+    pokemonCard_Detail.style.backgroundColor = backgroundColor(e.type.name);
   });
   typeData.classList.add("types");
   pokemonFront_Card.appendChild(typeData);
   pokemonFront_Card.classList.add("pokemonFrontCard");
 
-  // Creating element Dynamically for frontCard
+  // Creating element Dynamically for BackCard
   pokemonBack_Card.innerHTML = `
     <div class="pokeIdesBackCard">
       <p id="idNumber">#${pokemonData.id}</p>
-      <p id="dColor"></p>
+      <div id="bColor"><p>${pokemonData.stats[0].stat.name}</p>
+      <p>${pokemonData.stats[0].base_stat}<p></p>
+      </div>
     </div>
     <figure class="figureBackCard">
       <img src="${pokemonData.sprites.back_default}" alt="${pokemonData.name}">
@@ -106,6 +128,7 @@ function renderCard(pokemonData) {
     p.innerText = `${e.ability.name}`;
     abilityData.appendChild(p);
   });
+
   abilityData.classList.add("abilities");
   pokemonBack_Card.appendChild(abilityData);
   pokemonBack_Card.classList.add("pokemonBackCard");
@@ -132,14 +155,88 @@ async function filterSelectTypes() {
     const dataUrl = element.pokemon.url;
     const response = await fetch(dataUrl);
     const data = await response.json();
+    console.log(data);
+
+
+    // const monoType = data.filter((elem) => {
+    //   return elem.types.length == 1;
+    // });
+
+    // const doubleType = data.filter((elem) => {
+    //   return elem.types.length == 2;
+    // });
+
     renderCard(data);
+    // renderCard(doubleType);
   });
+}
+
+async function searchingPokemonByName() {
+  pokemonList.innerHTML = "";
+  let inputVal = searchPokemonInput.value;
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputVal}`);
+  const data = await response.json();
+  console.log(data);
+  renderCard(data);
 }
 
 searchFilter.addEventListener("click", (event) => {
   event.preventDefault();
   filterSelectTypes();
 });
+
+searchBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  searchingPokemonByName();
+});
+
+/*****************************************************************************************
+@param pokemonData
+
+getting reference from from renderCard function and
+return backGround Color
+
+@return color
+*****************************************************************************************/
+function backgroundColor(pokemonData) {
+  if (pokemonData == "normal") {
+    return "#CAD8CF";
+  } else if (pokemonData == "flying") {
+    return "#E1E1E1";
+  } else if (pokemonData == "fighting") {
+    return "#F75454";
+  } else if (pokemonData == "poison") {
+    return "#D86FB9";
+  } else if (pokemonData == "ground") {
+    return "#EBC174";
+  } else if (pokemonData == "rock") {
+    return "#B5904C";
+  } else if (pokemonData == "bug") {
+    return "#77A93A";
+  } else if (pokemonData == "ghost") {
+    return "#A595B1";
+  } else if (pokemonData == "steel") {
+    return "#D3D3D3";
+  } else if (pokemonData == "fire") {
+    return "#FF8800";
+  } else if (pokemonData == "water") {
+    return "#00A6FF";
+  } else if (pokemonData == "grass") {
+    return "#40FF00";
+  } else if (pokemonData == "electric") {
+    return "#FFFF00";
+  } else if (pokemonData == "psychic") {
+    return "#E764C9";
+  } else if (pokemonData == "ice") {
+    return "#33A0F8";
+  } else if (pokemonData == "dragon") {
+    return "#FFD700";
+  } else if (pokemonData == "dark") {
+    return "#8A8A8A";
+  } else if (pokemonData == "fairy") {
+    return "#FF7C92";
+  }
+}
 
 /***********************************
       window onLoad----->
